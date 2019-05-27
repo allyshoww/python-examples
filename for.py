093d6adbeb1a23ec85a3785170c15bbd21ad9533
Allyson de Souza Oliveira <asoliveira@outlook.com>
2019-05-27 16:32:47 -0300

    update for.py

--- for.py
+++ for.py
@@ -2,14 +2,16 @@
 
 num  = ["1", "2", "3", "4"]
 
-letras = 'Teste com For - Python'
+letras = []
+
+frase = 'Teste com For - Python'
 
 #Printar todos os numeros de uma lista
 for a in num:
     print(a)
 
 #Printar todas as lestras (string) de uma lista
-for a in letras:
+for a in frase:
     print(a)
     
 # Printar todos os numeros de um range de 1 a 10 (lembrando que nesse caso começa do 0)
@@ -23,3 +25,13 @@ for x in range(3,10):
 # Printar todos os numero de 1 a 10 de dois em dois (isso chama PASSO)    
 for x in range(1,10, 2):
     print (x)    
+    
+# Printar os numeros na sequencia inversa
+for y in range(100, 0, -2):
+    print(y)
+    
+# Printar o alfabeto e salvar na variavel letras
+for y in range(97, 97 + 26):
+    letras.append(chr(y))
+    
+    print(letras)
