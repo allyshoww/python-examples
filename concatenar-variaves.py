856121a2cd2bcbae21e66d630710b927f11c77d2
Allyson de Souza Oliveira <asoliveira@outlook.com>
2019-05-29 16:30:54 -0300

    updater files

--- concatenar-variaves.py
+++ concatenar-variaves.py
@@ -1,4 +1,5 @@
+#Une duas variaveis (nome,idade) em uma outra variavel onde o format substitui os valores das variaveis (nome e idade)
 nome, idade = 'Allyson', 31
 mensagem = 'Meu nome é {0} e minha idade é {1}'.format(nome,idade)
 #Printa mensagem no shell interativo
-mensagem
+print(mensagem)
