5a378b686dfc244c065d09ae8358e2b6f7bb38d9
Allyson de Souza Oliveira <asoliveira@outlook.com>
2019-06-03 09:56:52 -0300

    add module

---
+++ exemplos/modulos-instaladores-pacotes.py
@@ -0,0 +1,23 @@
+# Modulo é um arquivo com várias funções utilizáveis em outra parte do código
+# Repositorio oficial Python: https://pypi.python.org/pypi
+
+# Importar o modulo. module é o nome do modulo que você quer importar
+
+import module
+
+# Criar um modulo
+
+
+def mod():
+    print("Modulo 1")
+
+
+def mod2():
+    print("Módulo 2")
+
+
+# Ver informações sobre o modulo
+dir(module)
+
+# Chamar o modulo
+module.mod()
